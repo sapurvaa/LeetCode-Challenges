a49b64aceffad7d370d3f8f416928fe80452143b
Apurvaa <sapurvaa@yahoo.in>
2020-05-09 18:54:53 -0400

    Number Complement

---
+++ number_complement.py
@@ -0,0 +1,35 @@
+'''
+Problem:
+Given a positive integer num, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Example 1:
+
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+Example 2:
+
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+Constraints:
+
+    The given integer num is guaranteed to fit within the range of a 32-bit signed integer.
+    num >= 1
+    You could assume no leading zero bit in the integer’s binary representation.
+    This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+'''
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        binary_num = list(bin(num).replace("0b",""))   #converting to binary and replacing the "0b" character from the string
+        compliment_num = ""
+        for b in binary_num:
+            if b == "0":
+                compliment_num = compliment_num + "1"
+            else:
+                compliment_num = compliment_num + "0"
+        
+        return int(compliment_num,2)
