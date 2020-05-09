58f24641a8d275786570b25c0ba91f7eb4ded96e
Apurvaa <sapurvaa@yahoo.in>
2020-05-08 20:35:02 -0400

    updated the constraints

--- check_straight_line.py
+++ check_straight_line.py
@@ -1,3 +1,12 @@
+'''
+Constraints:
+
+    2 <= coordinates.length <= 1000
+    coordinates[i].length == 2
+    -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+    coordinates contains no duplicate point.
+'''
+
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         if len(coordinates) == 2:
